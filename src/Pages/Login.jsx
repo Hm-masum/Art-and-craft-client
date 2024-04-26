17578af0d863266b623b36c63ac250c341b18766
Hm-masum <hhmasum88@gmail.com>
2024-04-26 17:46:15 +0600

    login page commit

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password)
+  };
+
+  // handle googleLogin
+  // const handleGoogleSignIn = () => {
+  //   googleLogin().then(() => {
+  //     toast.success('User login successfully')
+  //     navigate(location?.state || "/");
+  //   });
+  // };
+
+  // handle githubSignIn
+  // const handleGithubSignIn = () => {
+  //   githubLogin().then(() => {
+  //     toast.success('User login successfully')
+  //     navigate(location?.state || "/");
+  //   });
+  // }
+
+  return (
+    <div className="bg-gray-100 rounded-xl p-4 md:p-10">
+      <h2 className="text-3xl mb-8 text-center font-semibold">Please Login</h2>
+      <div className="animate__animated animate__slideInLeft md:w-3/4 lg:w-1/2 mx-auto bg-white rounded-xl p-4">
+        <form onSubmit={handleLogin}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <div className="relative">
+              <input
+                className="w-full input input-bordered"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <span
+                className="absolute top-4 right-3"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </span>
+            </div>
+          </div>
+          <br />
+          <input
+            className="btn btn-accent w-full"
+            type="submit"
+            value="Login"
+          />
+        </form>
+        <p className="my-3">
+          Do not have an account please{" "}
+          <Link to={`/register`} className="text-blue-600 font-semibold">
+            Register
+          </Link>
+        </p>
+        <div className="divider">Continue With</div>
+        <div className="flex justify-center gap-4">
+          <button
+            // onClick={handleGoogleSignIn}
+            className="btn btn-outline btn-accent"
+          >
+            Google{" "}
+          </button>
+          <button
+            // onClick={handleGithubSignIn}
+            className="btn btn-outline btn-secondary"
+          >
+            Github{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+    ]
   },
 ]);
 
